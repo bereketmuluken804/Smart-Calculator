8ec18d62fc14eb736460df3be448916e0125a5ad
M. Bereket <bmuluken804@gmail.com>
2026-02-06 00:20:36 +0300

    Reset display after whena new digit is clicked

--- calculator.js
+++ calculator.js
@@ -1,19 +1,18 @@
 function add(a, b){
-    return Math.round((a + b)*1000)/1000;
+    return Math.round((a + b)*10000000000)/10000000000;
 }
 
 function subtract(a, b){
-    return Math.round((a - b) * 1000) / 1000;
+    return Math.round((a - b) * 10000000000) / 10000000000;
 }
 
 function multiply(a, b){
-    return Math.round((a * b) * 1000) / 1000;
+    return Math.round((a * b) * 10000000000) / 10000000000;
 }
 
 function divide(a, b){
     if (b === 0) return "Can't divide by zero";
-    num = a / b;
-    return Math.round(num * 1000) / 1000;
+    return Math.round((a / b) * 10000000000) / 10000000000;
 
 }
 
@@ -21,8 +20,8 @@ function swichSign(a){
     return -(a);
 }
 
-function percentage(a){
-    return a/100;
+function percentage(a, b){
+    return Math.round((a/100 * b) * 10000000000) / 10000000000;
 }
 
 let first, op, second;
@@ -38,7 +37,7 @@ function operate(first, op, second){
         case "-": return subtract(first, second);
         case "×": return multiply(first, second);
         case "÷": return divide(first, second);
-        case "%": return percentage(first);
+        case "%": return percentage(first, second);
         case "s": return swichSign(first); 
     }
 }
@@ -50,7 +49,8 @@ for (let i of keys){
     if ((!Number.isInteger(i))){
         btn.style.backgroundColor = "#0ed367"; 
         btn.classList.add("op");
-        if (["÷", "×", "-", "+"].includes(i)){btn.classList.add("basic_ops")}
+        if (["÷", "×", "-", "+", "%"].includes(i)){btn.classList.add("basic_ops")}
+        if(i === "+/-"){btn.classList.add("signChange")}
     }
     
     if (i === "=") {
@@ -136,29 +136,45 @@ numPad.addEventListener("click", e=>{
 }
 })
 
+let equal_clicked = false;
 const num_keys = document.querySelectorAll(".num_key");
 num_keys.forEach(key=>{
     key.addEventListener("click", e=>{
+        if(operation.textContent.length > 22){return}
+        if(equal_clicked){
+            operation.textContent = "";
+            result.textContent = "";
+        }
         if(operation.textContent === "0"){
             operation.textContent = e.target.textContent;
         }
-        else if(Number.isNaN(Number(operation.textContent)) && operation.textContent.at(-1) === "0"){
-            operation.textContent = operation.textContent.slice(0,-1) += e.target.textContent;
+        else if(operation.textContent.at(-1) === "0"){
+            if(operation.textContent.at(-2) !== "." && Number.isNaN(Number(operation.textContent.at(-2)))){
+                operation.textContent = operation.textContent.slice(0, -1) + e.target.textContent;
+            }
+            else{
+                operation.textContent += e.target.textContent
+            }
         }
         else {operation.textContent += e.target.textContent;}
+        equal_clicked = false;
     });
 });
 
 function equalOperation(){
     if(!(Number.isInteger(Number(operation.textContent)))){
-         if(!(["÷", "×", "-", "+", "%"].includes(operation.textContent.at(-1)))){
+        if(operation.textContent.at(-1) === "%"){
+                operation.textContent = Number(operation.textContent.slice(0, -1)) / 100;
+                return;
+            }
+        if(!(["÷", "×", "-", "+", "%"].includes(operation.textContent.at(-1)))){
         let cleanText = operation.textContent.replace(/\s+/g, ""); // remove all spaces
         let min = "";
         if(cleanText.at(0)==="-"){
             min = "-";
             cleanText = cleanText.slice(1);
         }
-        parts = cleanText.split(/([+\-×÷])/);
+        parts = cleanText.split(/([+\-×÷%])/);
         parts[0] = min+parts[0];
         let r = operate(parts[0], parts[1], parts[2]);
         if(r === "Can't divide by zero"){
@@ -167,17 +183,22 @@ function equalOperation(){
                 }
         operation.textContent = r;
         result.textContent = "";
+        equal_clicked = true;
     }
 }
 }
 
 function nextOperation(e){
-    if(operation.textContent.replace(/\s+/g, "").length < 25){
+    if(operation.textContent.replace(/\s+/g, "").length < 23){
         if(!(Number.isNaN(Number(operation.textContent)))){
             operation.textContent += e.target.textContent;
         }
         else{    
             if(["÷", "×", "-", "+", "%"].includes(operation.textContent.at(-1))){
+                if(operation.textContent.at(-1) === "%"){
+                    operation.textContent = Number(operation.textContent.slice(0, -1)) / 100;
+                    return;
+                }
                 operation.textContent = operation.textContent.slice(0, -1) + e.target.textContent;
             } 
             else{
@@ -187,7 +208,7 @@ function nextOperation(e){
                     min = "-";
                     cleanText = cleanText.slice(1);
                 }
-                parts = cleanText.split(/([+\-×÷])/);
+                parts = cleanText.split(/([+\-×÷%])/);
                 parts[0] = min+parts[0];
                 let r = operate(parts[0], parts[1], parts[2]);
                 if(r === "Can't divide by zero"){
@@ -198,6 +219,7 @@ function nextOperation(e){
                 result.textContent = r;
             }
         }
+        equal_clicked = false;
     }
 
 }
@@ -209,16 +231,33 @@ const equal = document.querySelector(".equal");
 equal.addEventListener("click",equalOperation)
 
 document.addEventListener("keydown", e=>{
+    e.preventDefault();
     if(["0","1","2","3","4","5","6","7","8","9"].includes(e.key)){
-        e.preventDefault();
-
+        if(operation.textContent.length > 22){return}
+        if(equal_clicked){
+            operation.textContent = "";
+            result.textContent = "";
+        }
         if(operation.textContent === "0"){
             operation.textContent = e.key;
         }
+        else if(operation.textContent.at(-1) === "0"){
+            if(operation.textContent.at(-2) !== "." && Number.isNaN(Number(operation.textContent.at(-2)))){
+                operation.textContent = operation.textContent.slice(0, -1) + e.key;
+            }
+            else{
+                operation.textContent += e.key
+            }
+        }
         else {operation.textContent += e.key;}
     }
-    else if(e.key === "=" || e.key==="Enter"){
-        equalOperation()
+    else if(e.key==="Enter"){
+        equalOperation();
+        return;
+    }
+    else if(e.key === "="){
+        equalOperation();
+        return;
     }
 
     else if(e.key ==="a" || e.key === "A" || (e.key === "Backspace" && e.ctrlKey) ){
@@ -231,7 +270,7 @@ document.addEventListener("keydown", e=>{
     }
 
     else if(["/", "*", "-", "+", "%"].includes(e.key)){
-        if(operation.textContent.replace(/\s+/g, "").length < 25){
+        if(operation.textContent.replace(/\s+/g, "").length < 23){
                 if(!(Number.isNaN(Number(operation.textContent)))){
                     if(e.key === "*"){operation.textContent += "×";}
                     else if(e.key === "/"){operation.textContent += "÷"}
@@ -257,6 +296,23 @@ document.addEventListener("keydown", e=>{
         }
     }
     }
+    else if(e.key === "."){
+        if(!Number.isNaN(Number(operation.textContent + "."))){
+        operation.textContent += ".";
+    }
+    else if(Number.isNaN(Number(operation.textContent))){
+        if(Number.isInteger(Number(operation.textContent.at(-1)))){
+            let cleanText = operation.textContent.replace(/\s+/g, "");// remove all spaces
+            parts = cleanText.split(/([+\-×÷])/);
+            if(!Number.isNaN(Number(parts[2] + "."))){
+                parts[2] += ".";
+                operation.textContent = parts.join("");
+
+            }
+        }
+    }
+    }
+    equal_clicked = false;
 });
 
 const dot = document.querySelector(".dot");
@@ -273,7 +329,15 @@ dot.addEventListener("click", e=>{
                 operation.textContent = parts.join("");
 
             }
-            operation.textContent = "".join(parts);
         }
     }
+    equal_clicked = false;
 })
+
+const signBtn = document.querySelector(".signChange");
+signBtn.addEventListener("click", e=>{
+    if(Number.isInteger(Number(operation.textContent))){
+        operation.textContent = -(Number(operation.textContent))
+    }
+})
+
